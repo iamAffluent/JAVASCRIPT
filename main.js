03b2c39f7d36308cde2c23459d14184431f0544b
iamAffluent <turnerbold@gmail.com>
2023-12-28 13:45:32 +0100

    day one progress

---
+++ main.js
@@ -0,0 +1,41 @@
+// console.log(5 % 3);
+
+// introduction to data type
+
+// primitive data type 
+// String 
+// number
+// Boolean
+// undefined
+// symbol
+
+// STRING EXAMPLE
+
+// "Strig with single quote";
+// "String with double quote"// string template literal doesn't break the line of quote when used it start and end
+// backtick
+// `BACKTICK`
+
+// NUMBER EXAMPLE
+234;
+34.2;
+5000;
+
+//BOOLEAN EXAMPLE 
+// True  false
+let House = 777
+
+console.log(typeof "three");
+console.log(typeof '687');
+console.log(typeof 687);
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof `bale`);
+console.log(typeof House);
+
+// undefine
+let Zawjah;
+console.log(Zawjah); //undefine mainly because the variable wasn't assigned any value
+
+// Variable Declaration
+// we can declare variable by using "const" and "let"
---
+++ readme.md
@@ -0,0 +1,4 @@
+JAVASCRIPT LESSON
+Hello!
+
+This repo will contain my javascript solution to 30 days of javascript by Asabeneh
